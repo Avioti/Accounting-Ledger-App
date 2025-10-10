c4c440822c3466925e61ce63ca324f4740bb1c2c
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-10 11:33:24 -0400

    created initial classes and transactions file

--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,4 +1,54 @@
 package com.pluralsight;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transactions {
+    private LocalDateTime date;
+    private String description, vendorName;
+    private double price;
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getdescription() {
+        return description;
+    }
+
+    public void setdescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Transactions(String vendorName, LocalDateTime date, String description, double price) {
+        this.vendorName = vendorName;
+        this.date = date;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss")) + "|" + description + "|" + vendorName + "|" + price ;
+    }
 }
