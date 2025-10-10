c8a1a43fe392d4365fabe54326589743551c7292
Avi <95044844+Avioti@users.noreply.github.com>
2025-10-10 16:33:22 -0400

    Added make payment and report screens

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,10 +1,13 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Application {
@@ -33,77 +36,159 @@ public class Application {
             switch (option) {
                 case 1:
 
+                    addDeposit();
+
                     break;
 
                 case 2:
 
+                    makePayment();
+
                     break;
 
                 case 3:
-                    Ledger();
+
+                    ledgerScreen();
 
                     break;
 
                 case 4:
+
                     storeActive = false;
 
                     break;
 
                 default:
+
                     System.out.println("Enter valid option");
 
             }
         }
     }
 
-    public static void Ledger() {
-        boolean inLedger = true;
 
-        while(inLedger){
-
-        System.out.println("Ledger screen");
-
+    public static void addDeposit() {
         Scanner scanner = new Scanner(System.in);
-        HashMap<LocalDateTime, Transactions> transactions = getTransaction();
 
-        System.out.println("1 - All");
-        System.out.println("2 - Deposits");
-        System.out.println("3 - Payments");
-        System.out.println("4 - Reports");
-        System.out.println("0 - Exit");
-
-        int option = scanner.nextInt();
+        System.out.println("Enter Deposit info");
+        System.out.print("Service Description: ");
+        String description = scanner.next().trim().toLowerCase();
+        scanner.nextLine();
+        System.out.print("Depositee: ");
+        String vendor = scanner.next().trim();
+        scanner.nextLine();
+        System.out.print("Total Deposited: ");
+        double price = scanner.nextDouble();
         scanner.nextLine();
 
-        switch (option) {
-            case 1:
-                displayAll();
+        HashMap<LocalDateTime, Transactions> transactions = getTransaction();
 
+        try {
 
-                break;
+            FileWriter fileWriter = new FileWriter("src/main/resources/transactions.csv", true);
 
-            case 2:
-            displayDeposit();
-                break;
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            bufWriter.newLine();
+            int invoiceNumber = 0;
+            invoiceNumber++;
 
-            case 3:
-                displayPayments();
+            String format = String.format(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + "|" + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "|" + "Invoice " + invoiceNumber + " paid " + "Service: %s|%s|%.2f", description, vendor, price);
+            bufWriter.write(format);
+            bufWriter.close();
 
-                break;
-
-            case 4:
-
-
-                break;
-
-            case 0:
-                inLedger = false;
-                break;
-
-            default:
-                System.out.println("Enter valid option");
+        } catch (Exception e) {
+            e.printStackTrace();
 
         }
+    }
+
+
+    public static void makePayment() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Deposit info");
+        System.out.print("Description: ");
+        String description = scanner.next().trim().toLowerCase();
+        scanner.nextLine();
+        System.out.print("Vendor: ");
+        String vendor = scanner.next().trim();
+        scanner.nextLine();
+        System.out.print("Total deducted: ");
+        double price = scanner.nextDouble();
+        scanner.nextLine();
+
+        HashMap<LocalDateTime, Transactions> transactions = getTransaction();
+
+        try {
+
+            FileWriter fileWriter = new FileWriter("src/main/resources/transactions.csv", true);
+
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            bufWriter.newLine();
+            String format = String.format(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + "|" + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "|" + "%s|%s|-%.2f", description, vendor, price);
+            bufWriter.write(format);
+            bufWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
+    public static void ledgerScreen() {
+        boolean inLedger = true;
+
+        while (inLedger) {
+
+            System.out.println("Ledger screen");
+
+            Scanner scanner = new Scanner(System.in);
+            HashMap<LocalDateTime, Transactions> transactions = getTransaction();
+
+            System.out.println("1 - All");
+            System.out.println("2 - Deposits");
+            System.out.println("3 - Payments");
+            System.out.println("4 - Reports");
+            System.out.println("0 - Exit to Home page");
+
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+
+                case 1:
+
+                    displayAll();
+
+                    break;
+
+                case 2:
+
+                    displayDeposit();
+
+                    break;
+
+                case 3:
+
+                    displayPayments();
+
+                    break;
+
+                case 4:
+
+                    reports();
+
+                    break;
+
+                case 0:
+                    inLedger = false;
+                    break;
+
+                default:
+                    System.out.println("Enter valid option");
+
+            }
         }
 
     }
@@ -118,7 +203,6 @@ public class Application {
 
             while ((input = buffReader.readLine()) != null) {
                 String[] transactionsDisplay = input.split("\\|");
-
 
 
                 LocalDate date = LocalDate.parse(transactionsDisplay[0]);
@@ -168,7 +252,7 @@ public class Application {
 
     }
 
-    public static void displayDeposit(){
+    public static void displayDeposit() {
         HashMap<LocalDateTime, Transactions> transactions = getTransaction();
 
         System.out.println("Deposits: ");
@@ -181,15 +265,15 @@ public class Application {
         for (LocalDateTime key : sortedKeys) {
             Transactions d = transactions.get(key);
 
-           if (d.getPrice() > 0){
-               System.out.println(d);
-           }
+            if (d.getPrice() > 0) {
+                System.out.println(d);
+            }
 
         }
 
     }
 
-    public static void displayPayments(){
+    public static void displayPayments() {
         HashMap<LocalDateTime, Transactions> transactions = getTransaction();
 
         System.out.println("Payments: ");
@@ -202,7 +286,7 @@ public class Application {
         for (LocalDateTime key : sortedKeys) {
             Transactions d = transactions.get(key);
 
-            if (d.getPrice() < 0){
+            if (d.getPrice() < 0) {
                 System.out.println(d);
             }
 
@@ -210,6 +294,86 @@ public class Application {
 
     }
 
+    public static void reports() {
+
+        boolean reportsScreen = true;
+
+        while (reportsScreen) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("1 - Month To Date");
+            System.out.println("2 - Previous Month");
+            System.out.println("3 - Year To Date");
+            System.out.println("4 - Previous Year");
+            System.out.println("5 - Search by Vendor");
+            System.out.println("0 - Back to Ledger Page");
+
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+
+                case 1:
+
+
+                    break;
+
+                case 2:
+
+
+                    break;
+
+
+                case 3:
+
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 0:
+                    reportsScreen = false;
+                    break;
+
+                default:
+                    System.out.println("Enter valid option");
+
+            }
+
+
+        }
+
+
+    }
+
+    public static String monthToDate() {
+
+        HashMap<LocalDateTime, Transactions> transactions = getTransaction();
+
+        System.out.println("Month to Date: ");
+
+
+        ArrayList<LocalDateTime> sortedKeys = new ArrayList<>(transactions.keySet());
+
+        Collections.sort(sortedKeys);
+
+
+        for (LocalDateTime key : sortedKeys) {
+            Transactions d = transactions.get(key);
+
+
+        }
+
+
+        return monthToDate();
+    }
 
 
 }
